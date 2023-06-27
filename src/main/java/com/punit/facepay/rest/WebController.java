97a63de2f7d9808f0bd3d8d6f3c48224cb286f90
Punit Jain <punit.15884@gmail.com>
2023-06-27 10:12:01 +0530

    added changes

--- src/main/java/com/punit/facepay/rest/WebController.java
+++ src/main/java/com/punit/facepay/rest/WebController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-	   @RequestMapping(value = "/index")
-	   public String index() {
-	      return "index";
-}
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/a")
+	public String admin() {
+		return "admin";
+	}
 
 
 }
