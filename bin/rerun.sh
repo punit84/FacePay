b61cb0d6027b7be3e7b627c105f592667c789723
Punit Jain <punit.15884@gmail.com>
2023-06-27 10:14:53 +0530

    added changes

---
+++ bin/rerun.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git pull origin master
+
+mvn clean package
+java -jar target/facepay-0.0.1-SNAPSHOT.jar
+
+
+# Find and kill Java process
+echo "Finding and killing Java process..."
+pid=$(pgrep -f "java -jar target/facepay-0.0.1-SNAPSHOT.jar")
+if [ -z "$pid" ]; then
+    echo "No Java process found."
+else
+    echo "Killing Java process with PID: $pid"
+    kill "$pid"
+fi
+
+# Execute another command
+echo "Executing another command..."
+# Replace the following line with your desired command
+# For example: java -jar your_jar_file.jar
+nohup java -jar target/facepay-0.0.1-SNAPSHOT.jar &
