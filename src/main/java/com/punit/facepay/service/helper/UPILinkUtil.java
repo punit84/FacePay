a07fab71c12f92ade273188999791b7f433e3a66
JAIN, PUNIT <jainpuni@amazon.com>
2023-08-15 12:43:35 +0530

    fixed url redirection issue

--- src/main/java/com/punit/facepay/service/helper/UPILinkUtil.java
+++ src/main/java/com/punit/facepay/service/helper/UPILinkUtil.java
@@ -8,9 +8,9 @@ public class UPILinkUtil {
 	public static String getUrl(String faceid) {
 		String responseSTR;
 		if (faceid.contains("@")) {
-			responseSTR = "upi://pay?pa="+faceid+"&pn=PaytmUser&cu=INR";
+			responseSTR = "Paytm://pay?pa="+faceid+"&pn=PaytmUser&cu=INR";
 		}else {
-			responseSTR = "upi://pay?pa="+faceid+"@paytm&pn=PaytmUser&cu=INR";
+			responseSTR = "Paytm://pay?pa="+faceid+"@paytm&pn=PaytmUser&cu=INR";
 		}
 		return responseSTR;
 	}
