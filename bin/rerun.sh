16f36cdee3b93211617e9128011f2d1e0409034e
Punit <jainpuni@amazon.com>
2024-05-07 09:40:56 +0530

    merged script to run awspe and QART

--- bin/rerun.sh
+++ bin/rerun.sh
@@ -5,10 +5,10 @@ git pull origin master
 mvn clean package
 
 # Find and kill Java process
-echo "Finding and killing Java process..."
+echo "Finding and killing AWSPE service process..."
 pid=$(pgrep -f "java -jar target/AWSPe-0.0.1-SNAPSHOT.jar")
 if [ -z "$pid" ]; then
-    echo "No Java process found."
+    echo "No AWSPE  process found."
 else
     echo "Killing Java process with PID: $pid"
     kill "$pid"
@@ -18,6 +18,26 @@ fi
 echo "Executing another command..."
 # Replace the following line with your desired command
 # For example: java -jar your_jar_file.jar
-nohup java -jar target/AWSPe-0.0.1-SNAPSHOT.jar &
+echo "Starting AWSPE..."
 
-tail -f logs/awspe.log
+nohup java -jar ../target/AWSPe-0.0.1-SNAPSHOT.jar &
+
+echo "Logs are available in tail -f logs/awspe.log"
+#tail -f logs/awspe.log
+
+
+echo  "Find the process ID (PID) of qart.py"
+PID=$(ps -ef | grep 'qart.py' | grep -v grep | awk '{print $2}')
+
+if [ -z "$PID" ]; then
+  echo "qart.py is not running."
+else
+  echo "Killing qart.py process (PID: $PID)..."
+  kill $PID
+fi
+
+# Run qart.py again in the background
+
+echo "Starting qart.py..."
+python3 qart.py &
+
